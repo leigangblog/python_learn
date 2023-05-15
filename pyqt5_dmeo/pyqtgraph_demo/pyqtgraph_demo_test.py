bf9df3b0039659a008e4dccdcbe8a2ab64f1cd79
leigangblog <1224365947@qq.com>
2023-05-15 21:11:19 +0800

    Description: 功能增加 feature or bugfix：bugfix Author:leigang Signed-off-by: leigangblog <1224365947@qq.com>

---
+++ pyqt5_dmeo/pyqtgraph_demo/pyqtgraph_demo_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:lei
+@file:pyqtgraph_demo_test.py
+@time:2023/05/15
+@邮箱：leigang431@163.com
+"""
+# pip install pyqtgraph
+import pyqtgraph.examples
+
+pyqtgraph.examples.run()
